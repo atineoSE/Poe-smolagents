44eec91d8c2258fde928f616bc0c723f989c3098
Adrian Tineo <adrian@adriantineo.com>
2025-08-26 18:11:12 +0200

    Restore example

--- simple_query.py
+++ simple_query.py
@@ -1,10 +1,5 @@
-import inspect
-
 from llm import model
 from poe_code_agent import PoeCodeAgent
-
-print(PoeCodeAgent.__module__)  # should point to your file
-print(inspect.getsource(PoeCodeAgent._generate_model_output))
 
 if __name__ == "__main__":
     agent = PoeCodeAgent(tools=[], model=model, verbosity_level=2)
