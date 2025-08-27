696b275fe204d433a66451f2f46eeb851b2c4940
Adrian Tineo <adrian@adriantineo.com>
2025-08-27 10:56:10 +0200

    Check for needed env vars

--- llm.py
+++ llm.py
@@ -11,8 +11,13 @@ load_dotenv()
 # e.g. DeepSeek-v3.1
 models.supports_stop_parameter = lambda model_id: False
 
+model_id = os.environ.get("MODEL")
+if not model_id:
+    raise ValueError("Could not find MODEL variable in the environment")
+api_key = os.environ.get("API_KEY")
+if not api_key:
+    raise ValueError("Could not find API_KEY variable in the environment")
+
 model = OpenAIServerModel(
-    model_id=os.environ.get("MODEL", ""),
-    api_base="https://api.poe.com/v1",
-    api_key=os.environ.get("API_KEY"),
+    model_id=model_id, api_base="https://api.poe.com/v1", api_key=api_key
 )
